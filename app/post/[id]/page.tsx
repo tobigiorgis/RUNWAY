8a215f3db1bd044e7010ab62b8eee3b5e079551a
tobigiorgis <tobiasgiorgis@uca.edu.ar>
2024-12-02 13:34:48 +0100

    no metadata

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -28,57 +28,57 @@ const Page = () => {
 
 export default Page
 
-type Props = {
-    params: { id: string }
-    searchParams: { [key: string]: string | string[] | undefined }
-  }
+// type Props = {
+//     params: { id: string }
+//     searchParams: { [key: string]: string | string[] | undefined }
+//   }
   
-export async function generateMetadata(
-    { params, searchParams }: Props,
-    parent: ResolvingMetadata
-  ): Promise<Metadata> {
-    // read route params
-    const postId = await searchParams.id as string
-    console.log(postId);
+// export async function generateMetadata(
+//     { params, searchParams }: Props,
+//     parent: ResolvingMetadata
+//   ): Promise<Metadata> {
+//     // read route params
+//     const postId = searchParams.id as string
+//     console.log(postId);
     
-    const supabase = createClient();
+//     const supabase = createClient();
    
-    // fetch data
-    const { data, error } = await supabase
-    .from('posts')
-    .select(`*, profiles(
-        username,
-        id
-        )`
-    )
-    .eq('id', postId)
+//     // fetch data
+//     const { data, error } = await supabase
+//     .from('posts')
+//     .select(`*, profiles(
+//         username,
+//         id
+//         )`
+//     )
+//     .eq('id', postId)
 
-      // handle error
-  if (error) {
-    console.error(error);
-    throw error;
-  }
+//       // handle error
+//   if (error) {
+//     console.error(error);
+//     throw error;
+//   }
 
-  // handle empty data
-if (!data || data.length === 0) {
-    throw new Error('No post found');
-}
+//   // handle empty data
+// if (!data || data.length === 0) {
+//     throw new Error('No post found');
+// }
 
-return {
-    // title: data && data[0]?.title,
-    // description: data && data[0]?.description,
-    // icons: [],
-    openGraph: {
-        title: `${data && data[0]?.title} by ${data && data[0]?.profiles.username} on Runway`,
-        description: `Take a look at this oufit!`,
-        url: `https://userunway.ar/post/${postId}`,
-        type: 'website',
-        images: [{
-            url: data && data[0]?.src,
-            width: 256,
-            height: 256,
-            alt: data && data[0]?.title,
-        }],
-    },
-}
-  }
+// return {
+//     // title: data && data[0]?.title,
+//     // description: data && data[0]?.description,
+//     // icons: [],
+//     openGraph: {
+//         title: `${data && data[0]?.title} by ${data && data[0]?.profiles.username} on Runway`,
+//         description: `Take a look at this oufit!`,
+//         url: `https://userunway.ar/post/${postId}`,
+//         type: 'website',
+//         images: [{
+//             url: data && data[0]?.src,
+//             width: 256,
+//             height: 256,
+//             alt: data && data[0]?.title,
+//         }],
+//     },
+// }
+//   }
