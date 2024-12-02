b7db70c8e628f30ad01ff23862f3bd6d138808e1
tobigiorgis <tobiasgiorgis@uca.edu.ar>
2024-12-02 15:00:04 +0100

    one more

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,53 +1,13 @@
-'use client'
+
 import React, { Suspense } from 'react'
-import Link from 'next/link';
-
-import { ForYou } from '@/components/DiscoverFeed/ForYou';
-import { Following } from '@/components/DiscoverFeed/Following';
-import { useSearchParams } from 'next/navigation';
+import DiscoverMain from '@/components/DiscoverFeed/DiscoverMain';
 
 
 
-
-const Discover = () => {
-
-    const searchParams = useSearchParams()
-    const tab = searchParams.get('tab') || 'forYou';
-
-  return (
-        <main
-            className={`flex min-h-screen pt-10 md:py-20 flex-col items-center bg-white`}
-        >
-            {/* <DiscoverState/> */}
-            <div className='flex flex-row md:w-fit h-fit mt-5 rounded-3xl items-center justify-between bg-gray '>
-                <Link 
-                    className={`w-fit my-1 ml-1 px-3 py-1 text-sm font-medium rounded-2xl ${ tab === 'forYou' ? 'bg-white text-gray-black font-semibold' : 'text-gray-500'}`} 
-                    href={`/discover?tab=forYou`}
-                >
-                    For you
-                </Link>
-                <Link 
-                    className={`w-fit my-1 mr-1 px-2 py-1 rounded-2xl text-sm font-medium ${ tab === 'following' ? 'bg-white text-gray-black font-semibold' : 'text-gray-500'}`} 
-                    href={`/discover?tab=following`}
-                >
-                    Following
-                </Link>
-            </div>
-        {
-            tab === 'forYou' ? (
-                <ForYou />
-            ) : (
-                <Following />
-            )
-        }
-        </main>
-  )
-}
-
-export function DiscoverPage() {
+export default function DiscoverPage() {
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <Discover />
+            <DiscoverMain />
         </Suspense>
     )
 }
