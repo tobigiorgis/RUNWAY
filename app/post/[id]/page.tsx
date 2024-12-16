db8c300d053cab73dca8905197a156530afe98b2
tobigiorgis <tobiasgiorgis@uca.edu.ar>
2024-12-16 18:20:36 +0100

    fix fix fix

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,13 +1,11 @@
 import { Metadata, ResolvingMetadata } from "next"
 import MainProductDetail from "@/components/ProductDetails/Main"
 
-const Page = ({
-  params,
-  // searchParams,
-}: {
-  params: { id: string };
-  // searchParams?: { [key: string]: string | undefined };
-}) => {
+type Props = {
+  params: { id: string }
+}
+
+const Page = ({params} : Props) => {
   
   // const postId = searchParams?.id || '';
   const postId = params.id;
