2274d0865a40eb9927ba6a1eaff74291c4b42b92
tobigiorgis <tobiasgiorgis@uca.edu.ar>
2024-12-17 22:07:43 +0100

    google log in

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,7 +8,11 @@ export async function GET(request: Request) {
 
   if (code) {
     const supabase = createClient();
-    await supabase.auth.exchangeCodeForSession(code);
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+    if (!error) {
+      return NextResponse.redirect(`${origin}`);
+    }
   }
 
   // URL to redirect to after sign in process completes
