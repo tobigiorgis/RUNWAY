e2bc8f3d6b911371596022d8b594c64e45aaf113
tobigiorgis <tobiasgiorgis@uca.edu.ar>
2024-12-18 09:41:18 +0100

    sign in fixed

--- app/actions.ts
+++ app/actions.ts
@@ -95,6 +95,7 @@ export async function signInWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
+      redirectTo: 'http://userunway.ar/auth/callback',
       queryParams: {
         access_type: "offline",
         prompt: "consent",
