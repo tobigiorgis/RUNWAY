13c5926db6f5990a2b93c8b06fd7e648a5dd7752
tobigiorgis <tobiasgiorgis@uca.edu.ar>
2024-12-18 10:51:56 +0100

    Update middleware.ts

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,7 +1,7 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { type NextRequest, NextResponse } from 'next/server';
 
-export const createClient = (request: NextRequest) => {
+export const createClient = async (request: NextRequest) => {
   // Create an unmodified response
   let response = NextResponse.next({
     request: {
@@ -56,6 +56,8 @@ export const createClient = (request: NextRequest) => {
       },
     }
   );
+  
+  await supabase.auth.getUser()
 
   return { supabase, response };
 };
